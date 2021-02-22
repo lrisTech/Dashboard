3de425eb55509de8b97dc243ddc353f1d4125cc3
Yuanming Chai <yaunmingchai@gmail.com>
2021-02-21 19:32:54 -0500

    init calender component

---
+++ client/src/components/dashboard/MainContent/GoogleCalender.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./MainContent.scss";
+import "./Dashboard.scss";
+import ReactDOM from 'react-dom';
+
+
+import { connect } from "react-redux";
+import ProgressBarEx from "../../widgets/progressbar/ProgressBar"
+
+class GoogleCalender extends Component {
+    render(){
+        
+    }
+}
+
+export default GoogleCalender
